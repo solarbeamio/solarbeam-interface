f589e0be4515ae088b1347e7b67fb5fe5d8dd7c7
- <->
2021-09-11 16:06:16 -0300

    faucet

--- src/pages/faucet/index.tsx
+++ src/pages/faucet/index.tsx
@@ -7,25 +7,78 @@ import Link from 'next/link'
 import { useLingui } from '@lingui/react'
 import { t } from '@lingui/macro'
 import { formatNumberScale } from '../../functions'
-import Button from '../../components/Button'
+import Button, { ButtonError } from '../../components/Button'
+import { AutoColumn } from '../../components/Column'
+import { AutoRow } from '../../components/Row'
+import DoubleGlowShadow from '../../components/DoubleGlowShadow'
+import SolarbeamLogo from '../../components/SolarbeamLogo'
+import Container from '../../components/Container'
+import Typography from '../../components/Typography'
+import { i18n } from '@lingui/core'
+import Image from '../../components/Image'
+import { useActiveWeb3React } from '../../hooks'
+import Web3Connect from '../../components/Web3Connect'
+import { Loader } from 'react-feather'
 
 export default function Faucet(): JSX.Element {
+  const { chainId, account, library } = useActiveWeb3React()
   return (
     <>
       <Head>
         <title>Faucet | Solarbeam</title>
-        <meta key="description" name="description" content="NFT Launchpad" />
+        <meta key="description" name="description" content="Moonriver Faucet" />
       </Head>
 
-      <div className="container px-0 mx-auto pb-6">
-        <div className={`mb-2 grid grid-cols-12 gap-4`}>
-          <div className="flex justify-center items-center col-span-12 lg:justify">
-            <Button color="gradient" className="text-emphasis text-yellow" variant={'flexed'} size={'nobase'}>
-              Faucet
-            </Button>
+      <SolarbeamLogo />
+      <Container maxWidth="2xl" className="space-y-6">
+        <DoubleGlowShadow>
+          <div className="p-4 space-y-4 rounded bg-dark-900" style={{ zIndex: 1 }}>
+            <div className="p-4 mb-3 space-y-3 text-center">
+              <Typography component="h1" variant="h2">
+                {i18n._(t`Moonriver Faucet`)}
+              </Typography>
+              <Typography component="h1" variant="base">
+                A Faucet is a tool that provides a small amount of MOVR to start using Solarbeam.io without having to
+                buy MOVR somewhere else.
+              </Typography>
+            </div>
+            <div className="flex flex-1 justify-center text-center items-center mt-8 mb-12">
+              <Image src="/images/faucet/moonriver-faucet.png" alt="Solarbeam" width={150} height={150} />
+            </div>
+            <div className="p-4 mb-3 space-y-3 text-center">
+              <Typography component="h1" variant="base">
+                Faucet balance: 0.1 MOVR
+              </Typography>
+            </div>
+            <AutoColumn gap={'md'}>
+              <div className={'flex items-center w-full'}>
+                {!account ? (
+                  <Web3Connect size="lg" color="gradient" className="w-full" />
+                ) : (
+                  <ButtonError
+                    className="font-bold text-light"
+                    onClick={() => {}}
+                    style={{
+                      width: '100%',
+                    }}
+                    disabled={false}
+                  >
+                    {false ? (
+                      <div className={'p-2'}>
+                        <AutoRow gap="6px" justify="center">
+                          Locking <Loader stroke="white" />
+                        </AutoRow>
+                      </div>
+                    ) : (
+                      i18n._(t`Send me some MOVR`)
+                    )}
+                  </ButtonError>
+                )}
+              </div>
+            </AutoColumn>
           </div>
-        </div>
-      </div>
+        </DoubleGlowShadow>
+      </Container>
     </>
   )
 }
