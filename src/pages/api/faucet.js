8a7359e872bd2f928c649d83c230087ff74c4e27
carrion9 <snadec@hotmail.com>
2021-09-11 23:01:27 +0300

    faucet api final

--- src/pages/api/faucet.js
+++ src/pages/api/faucet.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
 let inOrder = false
 let history = {
   ips: {},
-  wallets: {},
+  wallets: {}
 }
 
 async function faucet(request) {
@@ -25,19 +25,19 @@ async function faucet(request) {
       const receipt = await web3Request(address, amount)
       ret.push({
         status: 200,
-        message: `You have successfuly been sent ${amount} MOVR`,
+        message: `You have successfuly been sent ${amount} MOVR`
       })
     } catch (error) {
       ret.push({
         status: 429,
-        message: error.message,
+        message: error.message
       })
     }
     inOrder = false
   } else {
     ret.push({
       status: 429,
-      message: 'Processing too many orders, please try again in a moment',
+      message: 'Processing too many orders, please try again in a moment'
     })
   }
   return ret
@@ -45,17 +45,17 @@ async function faucet(request) {
 
 function checkLimit(request) {
   const address = request.query.address.toLowerCase()
+  const ip = request.connection.remoteAddress
   const timeLimit = parseInt(process.env.FAUCET_TIME_LIMIT_MIN) * 60 * 1000
   if (
-    // TODO need to find ip from request
-    (history.ips.hasOwnProperty(request.ip) && history.ips[request.ip] > Date.now() - timeLimit) ||
+    (history.ips.hasOwnProperty(ip) && history.ips[ip] > Date.now() - timeLimit) ||
     (history.wallets.hasOwnProperty(address) && history.wallets[address] > Date.now() - timeLimit)
   ) {
     throw new Error(
-      `You have reached the daily limit. <br> <b>Limit expires</b> in ${timeLeft(history.ips[request.ip])}`
+      `You have reached the daily limit. <br> <b>Limit expires</b> in ${timeLeft(history.ips[ip])}`
     )
   }
-  history.ips[request.ip] = Date.now()
+  history.ips[ip] = Date.now()
   history.wallets[address] = Date.now()
 }
 
@@ -75,36 +75,34 @@ function secondsToString(uptime) {
 }
 
 function timeLeft(timestamp) {
-  const timeNeeded = process.env.REDIS_EXPIRE_SECONDS * 1000
+  const timeNeeded = process.env.FAUCET_TIME_LIMIT_MIN * 60 * 1000
   const timePassed = Date.now() - timestamp
   const timeLeft = timeNeeded - timePassed
   return secondsToString(timeLeft / 1000)
 }
 
 async function web3Request(to, value) {
+  const wallet = await web3.eth.accounts.wallet.add(process.env.FAUCET_WALLET_PRIVATE_KEY)
+  const nonce = await web3.eth.getTransactionCount(wallet.address, 'pending')
+  const gasPrice = await web3.utils.toWei(process.env.FAUCET_GAS_PRICE_GWEI, 'gwei')
   return new Promise(async (resolve, reject) => {
     const transactionParams = {
-      //TODO fix nonce
-      nonce: this.nonce,
-      gasPrice: web3.utils.toHex(this.gasPrice),
+      nonce: web3.utils.toBN(nonce),
+      gasPrice: web3.utils.toHex(gasPrice),
       gasLimit: '0x9C40',
       to: to,
-      //TODO fix address
-      from: this.wallet.address,
-      value: web3.utils.toWei(`${value}`, 'ether'),
+      from: wallet.address,
+      value: web3.utils.toWei(`${value}`, 'ether')
     }
-    // TODO manage wallet privateKey
-    const signedTransaction = await web3.eth.accounts.signTransaction(transactionParams, this.wallet.privateKey)
+    const signedTransaction = await web3.eth.accounts.signTransaction(transactionParams, wallet.privateKey)
 
     web3.eth
       .sendSignedTransaction(signedTransaction.rawTransaction)
       .then((tx) => {
-        this.nonce = `${parseInt(this.nonce, 10) + 1}`
         console.log(tx)
         resolve(tx)
       })
       .catch((error) => {
-        this.nonce = `${parseInt(this.nonce, 10) + 1}`
         console.log(error)
         reject(error)
       })
