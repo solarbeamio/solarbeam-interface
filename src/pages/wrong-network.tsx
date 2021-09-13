2e8ee6be59ecd8ea54f499a0bafe5dbdb994e28c
- <->
2021-09-13 17:50:43 -0300

    fixed wrong-network

--- src/pages/wrong-network.tsx
+++ src/pages/wrong-network.tsx
@@ -1,7 +1,7 @@
 export default function WrongNetwork() {
   return (
     <h1 className="text-center">
-      Looks like you're using an unsupported network. <br /> Switch to Moonriver Network to use Solarbeam.io.
+      {`Looks like you're using an unsupported network. <br /> Switch to Moonriver Network to use Solarbeam.io.`}
     </h1>
   )
 }
