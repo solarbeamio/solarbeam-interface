b9a47662a4e98081e3bc38771606f68113e6cb6d
carrion9 <snadec@hotmail.com>
2021-09-21 05:03:47 +0300

    farms menu chinese fix

--- src/features/farm/FarmMenu.tsx
+++ src/features/farm/FarmMenu.tsx
@@ -16,7 +16,7 @@ const MenuItem = ({ href, title }) => {
       activeClassName="font-bold bg-transparent border rounded text-high-emphesis border-transparent border-gradient-r-purple-dark-900"
     >
       <a className="flex items-center justify-between px-6 py-6  text-base font-bold border border-transparent rounded cursor-pointer bg-dark-800">
-        {i18n._(t`${title}`)}
+        {title}
       </a>
     </NavLink>
   )
@@ -27,8 +27,8 @@ const Menu = ({ positionsLength, onSearch, term }) => {
   return (
     <div className={`grid grid-cols-12`}>
       <div className="col-span-12 flex flex-col space-y-4">
-        <MenuItem href="/farm" title="All Farms" />
-        {account && positionsLength > 0 && <MenuItem href={`/farm?filter=my`} title={`My Farms`} />}
+        <MenuItem href="/farm" title={i18n._(t`All Farms`)} />
+        {account && positionsLength > 0 && <MenuItem href={`/farm?filter=my`} title={i18n._(t`My Farms`)} />}
 
         {/* <MenuItem href="/farm?filter=solar" title="SOLAR Farms" />
         <MenuItem href="/farm?filter=moonriver" title="MOVR Farms" />
