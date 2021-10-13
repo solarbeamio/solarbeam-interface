63dcf75b18605306dfd3bf3c5c690b5e0c18043f
- <->
2021-10-13 11:44:53 -0300

    fixed lp link

--- src/components/Header/More.tsx
+++ src/components/Header/More.tsx
@@ -13,7 +13,7 @@ const items = (i18n: I18n) => [
   {
     name: i18n._(t`Website`),
     description: i18n._(t`Solarbeam.io landing page`),
-    href: 'https://solarbeam.io',
+    href: 'https://solarbeam.io/home',
     external: true,
   },
   {
