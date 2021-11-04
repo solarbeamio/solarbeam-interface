38f58157c3bbef34f3ed8945d353bfe52af8de4a
- <->
2021-11-04 09:34:39 -0300

    vaults improvements

---
+++ src/features/vault/VaultSidebar.tsx
@@ -0,0 +1,75 @@
+import React, { useContext } from 'react'
+import { useActiveWeb3React } from '../../hooks'
+import { useLingui } from '@lingui/react'
+import { formatNumber } from '../../functions'
+import { usePositions } from './hooks'
+import { PriceContext } from '../../contexts/priceContext'
+import Typography from '../../components/Typography'
+
+export const Sidebar = ({ tvl, vaults }) => {
+  const { chainId } = useActiveWeb3React()
+  const { i18n } = useLingui()
+  const positions = usePositions()
+
+  const priceData = useContext(PriceContext)
+
+  const solarPrice = priceData?.solar
+
+  let summTvl = tvl.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue.tvl
+  }, 0)
+
+  let summTvlVaults = vaults.reduce((previousValue, currentValue) => {
+    return previousValue + (currentValue.totalLp / 1e18) * solarPrice
+  }, 0)
+
+  const valueStaked = positions.reduce((previousValue, currentValue) => {
+    return previousValue + (currentValue.amount / 1e18) * solarPrice
+  }, 0)
+
+  return (
+    <div className={`flex flex-col px-6 py-6 space-y-6 shadow bg-dark-700 sm:bg-light-glass rounded-xxl`}>
+      <div>
+        <Typography variant="h3" className={'text-high-emphesis'}>
+          Vaults
+        </Typography>
+        <Typography variant="base" className={'text-emphasis'}>
+          Solar Vaults are high incentivized pools. Long term supporters can choose to lock SOLAR for a determined
+          period for higher rewards and increased multipliers for Eclipse.{' '}
+          <a
+            className="text-purple cursor-pointer"
+            href="https://docs.solarbeam.io/getting-started/vaults"
+            rel="noreferrer"
+            target="_blank"
+          >
+            See how it works.
+          </a>
+        </Typography>
+      </div>
+      <div>
+        <Typography variant="base" className={'text-emphasis'}>
+          Total Value Locked
+        </Typography>
+        <Typography variant="h3" className={'text-high-emphesis'}>
+          {formatNumber(summTvl + summTvlVaults, true)}
+        </Typography>
+      </div>
+      <div>
+        <Typography variant="base" className={'text-emphasis'}>
+          Vaults TVL
+        </Typography>
+        <Typography variant="h3" className={'text-high-emphesis'}>
+          {formatNumber(summTvlVaults, true)}
+        </Typography>
+      </div>
+      <div>
+        <Typography variant="base" className={'text-emphasis'}>
+          My Holdings
+        </Typography>
+        <Typography variant="h3" className={'text-high-emphesis'}>
+          {formatNumber(valueStaked, true)}
+        </Typography>
+      </div>
+    </div>
+  )
+}
--- src/pages/vaults/index.tsx
+++ src/pages/vaults/index.tsx
@@ -19,11 +19,15 @@ import useMasterChef from '../../features/farm/useMasterChef'
 import { useTVL } from '../../hooks/useV2Pairs'
 import { getAddress } from '@ethersproject/address'
 import VaultList from '../../features/vault/VaultList'
+import { Sidebar } from '../../features/vault/VaultSidebar'
+import Menu from '../../features/vault/VaultMenu'
 
 export default function Vault(): JSX.Element {
   const { i18n } = useLingui()
   const router = useRouter()
   const { chainId } = useActiveWeb3React()
+
+  const type = router.query.filter as string
 
   const positions = usePositions()
 
@@ -122,7 +126,14 @@ export default function Vault(): JSX.Element {
       blocksPerHour,
     }
   }
-  const data = vaults.map(map)
+
+  const FILTER = {
+    my: (farm) => farm?.amount && !farm.amount.isZero(),
+  }
+
+  const data = vaults.map(map).filter((farm) => {
+    return type in FILTER ? FILTER[type](farm) : true
+  })
 
   const valueStaked = positions.reduce((previousValue, currentValue) => {
     return previousValue + (currentValue.amount / 1e18) * solarPrice
@@ -136,51 +147,18 @@ export default function Vault(): JSX.Element {
       </Head>
 
       <DoubleGlowShadow opacity="0.6" maxWidth={false} className={'container px-0 mx-auto '}>
-        <div className={`z-4 grid grid-cols-12 md:space-x-4 space-y-4 md:space-y-0 sm:pb-12 sm:pt-12 sm:mt-16 `}>
-          <div className={`col-span-12`}>
-            <Card className="bg-dark-900 z-4">
-              <div className={`grid grid-cols-12 md:space-x-4 space-y-4 md:space-y-0 `}>
-                <div className={`col-span-12 md:col-span-3 space-y-4`}>
-                  <div className={`hidden md:block`}>
-                    <div className={`col-span-12 md:col-span-4 bg-dark-800 px-6 py-4 rounded`}>
-                      <div className="mb-2 text-2xl text-emphesis">{i18n._(t`Solar Vault`)}</div>
-                      <div className="mb-4 text-base text-secondary">
-                        <p>
-                          {i18n._(
-                            t`Solar Vault is a set of high incentivized pools. Long term supporters can choose to lock SOLAR for a determined period for higher rewards.`
-                          )}
-                        </p>
-                        <p className="mt-2">
-                          {i18n._(
-                            t`The participants receive various benefits such as higher rewards according to lock duration, higher allocations in Solar Launchpad and more.`
-                          )}
-                        </p>
-                      </div>
-                      <div className="mb-2 text-2xl text-emphesis">{i18n._(t`Considerations`)}</div>{' '}
-                      <div className="mb-4 text-base text-secondary">
-                        <p>{i18n._(t`Everytime you stake or claim rewards your lock time renews.`)}</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className={`flex flex-col items-center justify-between px-6 py-6 `}>
-                    <div className="flex items-center justify-between py-2 text-emphasis">
-                      Total Value Locked: {formatNumberScale(summTvl + summTvlVaults, true, 2)}
-                    </div>
-                    <div className="flex items-center justify-between py-2 text-emphasis">
-                      Vaults TVL: {formatNumberScale(summTvlVaults, true, 2)}
-                    </div>
-                    {positions.length > 0 && (
-                      <div className="flex items-center justify-between py-2 text-emphasis">
-                        My Holdings: {formatNumberScale(valueStaked, true, 2)}
-                      </div>
-                    )}
-                  </div>
-                </div>
-                <div className={`col-span-12 md:col-span-9 bg-dark-800  py-4 md:px-6 md:py-4 rounded`}>
-                  <VaultList farms={data} />
-                </div>
+        <div className={`z-4 grid grid-cols-12 md:space-x-4 space-y-4 md:space-y-0 pb-12 sm:pt-12 sm:mt-16 mt-4 m-2 `}>
+          <div className={`col-span-12 md:col-span-3 space-y-4`}>
+            <Sidebar tvl={tvlInfo} vaults={vaults} />
+          </div>
+          <div className={`col-span-12 md:col-span-9 py-4 md:px-6 md:py-6 shadow bg-dark-900 rounded-xxl`}>
+            <div className={`flex flex-col md:flex-row space-y-4 md:space-x-5 md:space-y-0 mb-8`}>
+              <Menu positionsLength={positions.length} />
+              <div className={'py-4 flex flex-1 text-right text-gray-500 justify-end items-center'}>
+                Everytime you stake or claim rewards your lock time renews
               </div>
-            </Card>
+            </div>
+            <VaultList farms={data} />
           </div>
         </div>
       </DoubleGlowShadow>
