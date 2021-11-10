e0aea4e50aeaab323544052c4e9ebe025b65b772
carrion9 <snadec@hotmail.com>
2021-11-11 01:59:17 +0200

    add APR details

--- src/features/farm/FarmListItem.tsx
+++ src/features/farm/FarmListItem.tsx
@@ -11,6 +11,7 @@ import CurrencyLogo from '../../components/CurrencyLogo'
 import { isMobile } from 'react-device-detect'
 import YieldDetails from '../../components/YieldDetails'
 import IconWrapper from '../../components/IconWrapper'
+import QuestionHelper from '../../components/QuestionHelper'
 import { Info } from 'react-feather'
 import Link from 'next/link'
 
@@ -26,64 +27,64 @@ const FarmListItem = ({ farm, ...rest }) => {
     <React.Fragment>
       <Disclosure {...rest}>
         {({ open }) => (
-          <div className="mb-4">
+          <div className='mb-4'>
             <Disclosure.Button
               className={classNames(
                 open && 'rounded-b-none',
                 'w-full px-4 py-6 text-left rounded-xxl cursor-pointer select-none bg-dark-700  text-primary text-sm md:text-lg'
               )}
             >
-              <div className="grid grid-cols-4 ">
-                <div className="flex col-span-2 space-x-4 md:col-span-1">
+              <div className='grid grid-cols-4 '>
+                <div className='flex col-span-2 space-x-4 md:col-span-1'>
                   {token1 ? (
                     <DoubleLogo currency0={token0} currency1={token1} size={isMobile ? 24 : 40} />
                   ) : (
-                    <div className="flex items-center">
+                    <div className='flex items-center'>
                       <CurrencyLogo currency={token0} size={isMobile ? 32 : 50} />
                     </div>
                   )}
 
                   <div className={`flex flex-col justify-center ${token1 ? 'md:flex-row' : ''}`}>
                     <div>
-                      <span className="flex font-bold">{farm?.pair?.token0?.symbol}</span>
-                      {token1 && <span className="flex font-bold">{farm?.pair?.token1?.symbol}</span>}
+                      <span className='flex font-bold'>{farm?.pair?.token0?.symbol}</span>
+                      {token1 && <span className='flex font-bold'>{farm?.pair?.token1?.symbol}</span>}
                       {!token1 && token0?.symbol == 'SOLAR' && (
-                        <div className="flex flex-col">
-                          <Link passHref href="/vaults">
-                            <span className="text-emphasis underline hover:text-yellow">Use Vaults</span>
+                        <div className='flex flex-col'>
+                          <Link passHref href='/vaults'>
+                            <span className='text-emphasis underline hover:text-yellow'>Use Vaults</span>
                           </Link>
                         </div>
                       )}
                     </div>
                   </div>
                 </div>
-                <div className="flex flex-col justify-center font-bold">{formatNumberScale(farm.tvl, true, 2)}</div>
-                <div className="flex-row items-center hidden space-x-4 md:flex">
-                  <div className="flex items-center space-x-2">
+                <div className='flex flex-col justify-center font-bold'>{formatNumberScale(farm.tvl, true, 2)}</div>
+                <div className='flex-row items-center hidden space-x-4 md:flex'>
+                  <div className='flex items-center space-x-2'>
                     {farm?.rewards?.map((reward, i) => (
-                      <div key={i} className="flex items-center">
+                      <div key={i} className='flex items-center'>
                         <Image
                           src={reward?.icon}
                           width={farm?.rewards?.length > 1 ? 40 : 50}
                           height={farm?.rewards?.length > 1 ? 40 : 50}
-                          className="rounded-md"
-                          layout="fixed"
+                          className='rounded-md'
+                          layout='fixed'
                           alt={reward?.token}
                         />
                       </div>
                     ))}
                   </div>
-                  <div className="flex flex-col space-y-1">
+                  <div className='flex flex-col space-y-1'>
                     {farm?.rewards?.map((reward, i) => (
-                      <div key={i} className="text-xs md:text-sm whitespace-nowrap">
+                      <div key={i} className='text-xs md:text-sm whitespace-nowrap'>
                         {formatNumber(reward.rewardPerDay)} {reward.token} {i18n._(t`/ DAY`)}
                       </div>
                     ))}
                   </div>
                 </div>
-                <div className="flex flex-col items-end justify-center">
+                <div className='flex flex-col items-end justify-center'>
                   <div
-                    className="font-bold flex justify items-center text-righttext-high-emphesis"
+                    className='font-bold flex justify items-center text-righttext-high-emphesis'
                     onClick={(e) => {
                       // e.stopPropagation()
                       // setSelectedFarm(farm.id)
@@ -92,9 +93,31 @@ const FarmListItem = ({ farm, ...rest }) => {
                     {/* <IconWrapper size="16px" marginRight={'10px'}>
                       <Info />
                     </IconWrapper> */}
+                    {!!farm?.feeAprPerYear && (
+                      <IconWrapper size='16px' marginRight={'10px'}>
+                        <QuestionHelper
+                          text={
+                            <div className='flex flex-col'>
+                              <div>
+                                Reward APR:{' '}
+                                {farm?.tvl !== 0
+                                  ? farm?.rewardAprPerYear > 10000
+                                    ? '>10,000%'
+                                    : formatPercent(farm?.rewardAprPerYear * 100)
+                                  : 'Infinite'}
+                              </div>
+                              <div>
+                                Fee APR:{' '}
+                                {farm?.feeAprPerYear < 10000 ? formatPercent(farm?.feeAprPerYear * 100) : '>10,000%'}
+                              </div>
+                            </div>
+                          }
+                        />
+                      </IconWrapper>
+                    )}
                     {farm.royPerYear > 1000000 ? '100000000%+' : formatPercent(farm.roiPerYear * 100)}
                   </div>
-                  <div className="text-xs text-right md:text-base text-secondary">{i18n._(t`annualized`)}</div>
+                  <div className='text-xs text-right md:text-base text-secondary'>{i18n._(t`annualized`)}</div>
                 </div>
               </div>
             </Disclosure.Button>
--- src/pages/farm/index.tsx
+++ src/pages/farm/index.tsx
@@ -12,7 +12,7 @@ import { POOLS, POOLS_V2 } from '../../constants/farms'
 import { useVaults } from '../../features/vault/hooks'
 import Search from '../../components/Search'
 import { Sidebar } from '../../features/farm/FarmSidebar'
-import useFeeAPR from '../../hooks/useFeeAPR'
+import useFeesAprPerYear from '../../hooks/useFeeAPR'
 
 export default function Farm(): JSX.Element {
   const { chainId } = useActiveWeb3React()
@@ -32,44 +32,38 @@ export default function Farm(): JSX.Element {
   const positions = usePositions()
   const positionsV2 = usePositionsV2()
 
-  const feeAPR = useFeeAPR(POOLS)
-  const feeAPRV2 = useFeeAPR(POOLS_V2)
+  const feesAprPerYear = useFeesAprPerYear(POOLS)
+  const feesAprPerYearV2 = useFeesAprPerYear(POOLS_V2)
 
-  const map = (pool, poolVersion, positions, pairPrices, feeAPR, version) => {
+  const map = (pool, poolVersion, positions, pairPrices, feesAprPerYear, version) => {
     const pair = poolVersion[chainId][pool.lpToken]
     const position = positions.find((position) => position.id === pool.id)
     const pairPrice = pairPrices.find((item) => item.token == pool.lpToken)
-    const fee = feeAPR.find((item) => item.id === pool.lpToken?.toLowerCase())
+    const feeAprPerYear = feesAprPerYear.find((item) => item.id === pool.lpToken?.toLowerCase())?.apr / 100
     const tvl = (pool?.totalLp / 10 ** pair?.decimals) * pairPrice?.price
-    const roiPerYear = fee?.apr
-      ? (pool?.rewards?.reduce((previousValue, currentValue) => {
-          return previousValue + currentValue.rewardPerDay * currentValue.rewardPrice
-        }, 0) /
-          tvl) *
-          365 +
-        fee?.apr / 100
-      : (pool?.rewards?.reduce((previousValue, currentValue) => {
-          return previousValue + currentValue.rewardPerDay * currentValue.rewardPrice
-        }, 0) /
-          tvl) *
-        365
-
     const rewardsTotal = pool?.rewards?.reduce((previousValue, currentValue) => {
       return previousValue + currentValue.rewardPerDay * currentValue.rewardPrice
     }, 0)
-
+    const rewardAprPerYear = (pool?.rewards?.reduce((previousValue, currentValue) => {
+        return previousValue + currentValue.rewardPerDay * currentValue.rewardPrice
+      }, 0) /
+      tvl) *
+      365
+    const roiPerYear = feeAprPerYear ? feeAprPerYear + rewardAprPerYear : rewardAprPerYear
+    console.log(roiPerYear, feeAprPerYear, rewardAprPerYear)
     return {
       ...pool,
       ...position,
       tvl,
       price: pairPrice?.price,
+      feeAprPerYear,
+      rewardAprPerYear,
       roiPerYear,
       rewardsTotal,
       pair: {
         ...pair,
         decimals: 18,
       },
-      feeAPR: fee?.apr,
       version,
     }
   }
@@ -79,8 +73,8 @@ export default function Farm(): JSX.Element {
     inactive: (farm) => farm?.rewardsTotal == 0,
   }
 
-  const itemsV1 = farms.map((item) => map(item, POOLS, positions, pairPrices, feeAPR, 1))
-  const itemsV2 = farmsV2.map((item) => map(item, POOLS_V2, positionsV2, pairPricesV2, feeAPRV2, 2))
+  const itemsV1 = farms.map((item) => map(item, POOLS, positions, pairPrices, feesAprPerYear, 1))
+  const itemsV2 = farmsV2.map((item) => map(item, POOLS_V2, positionsV2, pairPricesV2, feesAprPerYearV2, 2))
 
   const items = itemsV1.concat(itemsV2)
 
